79dd0177cc5a5ae4f0e59b04609026fff99e548d
Carlo Quinonez <Carlo@studiofathom.com>
2017-09-08 13:53:32 -0700

    WIP

---
+++ config.js
@@ -0,0 +1,7 @@
+Shift = {};
+
+Shift.config = {
+  shiftURL : "http://localhost:3000/",
+  agentRole: "agent",
+  serverDelay : 1000
+};
--- shift-microserver.js
+++ shift-microserver.js
@@ -1,35 +1,36 @@
 // Author Carlo Quinonez
 // Description Start the Fusion360 Microserver for Shift.
 
-import { config } from './config.json';
-
 function run(context) {
-    // create cookie for this agent.
-    var dirPath = adsk.tempDirectory() + '/shift';
-    var cookiePath = dirPath + '/cookie';
+    // create unique dir for this agent.
+    var adskEmail = adsk.core.Application.get().userName;
+    var adskId = adsk.core.Application.get().userId;
 
-    if (!adsk.fileIsDirectory(dirPath)) {
-        console.log('Creating Shift directory at ', shiftDir);
-        adsk.createDiurectory(shiftDir + '/shift');
-    }
+    var cookie = uuidv4();
+    var shiftDir = adsk.tempDirectory() + '/shift/' + '/' + cookie;
+    adsk.createDirectory(shiftDir + '/shift');
 
-    if (!adsk.fileExists(cookiePath)) {
-        console.log('Creating cookie at ', cookiePath);
-        adsk.writeFile(cookiePath, uuidv4());
-    }
+    var shiftURL =
+        Shift.config.shiftURL +
+        '/' +
+        Shift.config.agentRole +
+        '?cookie=' +
+        encodeURIComponent(cookie) +
+        '&adskEmail=' +
+        encodeURIComponent(adskEmail) +
+        '&adskId=' +
+        encodeURIComponent(adskId);
 
-    var cookie = adsk.readFile(cookiePath);
-    console.log('Cookie id ', cookie);
-
-    var shiftURL = config.shiftURL + '?cookie=' + encodeURIComponent(cookie);
     console.log('Starting Shift microserver from URL ', shiftURL);
 
-    window.setTimeout(startMicroserver, config.serverDelay)
+    window.setTimeout(startMicroserver, Shift.config.serverDelay);
 
     function uuidv4() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0,
-                v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(
+            c
+        ) {
+            var r = (Math.random() * 16) | 0,
+                v = c == 'x' ? r : (r & 0x3) | 0x8;
             return v.toString(16);
         });
     }
